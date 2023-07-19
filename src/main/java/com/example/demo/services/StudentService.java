63ad9bb401a892399174f8960a4c116146da6ecb
NatswarChuan <natswarchuan@gmail.com>
2023-07-19 11:17:24 +0700

    .

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -5,10 +5,11 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Student;
+import com.example.demo.interfaces.Services.IStudentService;
 
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class StudentService extends AbService<Student, UUID> {
+public class StudentService extends AbService<Student, UUID> implements IStudentService {
 }
