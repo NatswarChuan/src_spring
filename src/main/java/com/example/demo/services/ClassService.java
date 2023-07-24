f06d56490bc4b4485f9a6dd85104b50d90f428d9
NatswarChuan <natswarchuan@gmail.com>
2023-07-24 10:59:49 +0700

    .

--- src/main/java/com/example/demo/services/ClassService.java
+++ src/main/java/com/example/demo/services/ClassService.java
@@ -3,7 +3,6 @@ package com.example.demo.services;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
